b629fc442cde638df04bd487073bd1646006171c
joe.wang <joe.wang@imaginato.com>
2018-01-05 19:48:46 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//0001
+// document.getElementById('app').innerHTML = "Webpack worked"
+
+//0002
+/*使用es6的箭头函数*/
+// var func = str => {
+//     document.getElementById('app').innerHTML = str;
+// };
+// func('我现在在使用Babel!');
+
+//0003
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+<div>Hello React!</div>, document.getElementById('app'));
---
+++ webpack.dev.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    //part-01
+    /*入口*/
+    entry: path.join(__dirname, 'src/index.js'),
+
+    /*输出到dist文件夹，输出文件名字为bundle.js*/
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+
+    //part-02
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: ['babel-loader?cacheDirectory=true'],
+            include: path.join(__dirname, 'src')
+        }]
+    },
+};
