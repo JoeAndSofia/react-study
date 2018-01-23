84b13f971cb190b63a3225843493de1084fed649
joe.wang <joe.wang@imaginato.com>
2018-01-23 18:54:19 +0800

    temp commit for sync

--- src/index.js
+++ src/index.js
@@ -9,8 +9,17 @@
 // func('我现在在使用Babel!');
 
 //0003
+// import React from 'react';
+// import ReactDom from 'react-dom';
+//
+// ReactDom.render(
+// <div>Hello React!</div>, document.getElementById('app'));
+
+//0004
 import React from 'react';
 import ReactDom from 'react-dom';
+import Hello from './component/Hello/Hello'
 
 ReactDom.render(
-<div>Hello React!</div>, document.getElementById('app'));
+	<Hello/>, document.getElementById('app')
+);
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
-
+console.log("__dirname", __dirname)
 module.exports = {
     //part-01
     /*入口*/
+    
     entry: path.join(__dirname, 'src/index.js'),
 
     /*输出到dist文件夹，输出文件名字为bundle.js*/
