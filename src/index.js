be461485ef91ebe63c12d082b802fe65e0d15d25
BearJoeWYL <wylsniper@163.com>
2018-03-05 17:55:20 +0800

    master 2018-03-05 17:51:56 1. remove module.hot from src/index/js 2. add react-hot-loader for keeping react state on page side and add config in both .babelrc and webpack.dev.config.js

--- src/index.js
+++ src/index.js
@@ -39,9 +39,9 @@ import ReactDom from 'react-dom';
 
 import getRouter from './router/router';
 
-if(module.hot) {
-	module.hot.accept();
-}
+// if(module.hot) {
+// 	module.hot.accept();
+// }
 
 ReactDom.render(
 	getRouter(), document.getElementById('app'));
