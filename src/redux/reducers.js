11fb87a30b00070fdab2da5ea9165b9b1693709f
BearJoeWYL <wylsniper@163.com>
2018-03-14 18:07:50 +0800

    master 2018-03-14 18:06:51 userInfo state operating: action and reducer management redux-thunk added

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,9 @@
-import counter from 'reducers/reducer_counter';
+import reducer_counter from 'reducers/reducer_counter';
+import reducer_userInfo from 'reducers/reducer_userInfo';
 
 export default function combineReducers(state = {}, action) {
 	return {
-		counter: counter(state.counter, action)
+		counter: reducer_counter(state.counter, action),
+		userInfo: reducer_userInfo(state.userInfo, action),
 	}
 }
